53c30214ac5a0d2b550bf7fb86290dd6b51125a2
EstebanHernandez1 <33496976+EstebanHernandez1@users.noreply.github.com>
2020-03-16 16:53:38 -0400

    Basic GUI layout

---
+++ MolecularGUI/app/src/main/java/com/example/esteb/moleculargui/MoleculeDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.example.esteb.moleculargui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoleculeDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_molecule_details);
+    }
+}
